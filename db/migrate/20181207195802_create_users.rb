00e9097204f23145bed314593e87d5e21f0c883d
josephpcoyne <josephpcoyne@gmail.com>
2018-12-07 15:00:41 -0500

    adds users model

---
+++ db/migrate/20181207195802_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.date :birthdate
+      t.string :gender
+      t.integer :weight
+      t.integer :height
+      t.string :avatar
+      t.string :bio
+      t.string :level
+
+      t.timestamps
+    end
+  end
+end
