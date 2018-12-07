dcdbb7399d42a2d31671dff627fd01c02b98e9a2
Joseph Coyne <39542045+josephpcoyne@users.noreply.github.com>
2018-12-07 16:06:00 -0500

    Update 20181207201516_create_meals.rb

--- db/migrate/20181207201516_create_meals.rb
+++ db/migrate/20181207201516_create_meals.rb
@@ -2,8 +2,7 @@ class CreateMeals < ActiveRecord::Migration[5.2]
   def change
     create_table :meals do |t|
       t.interger :user_id
-      t.string :recipe_name
-      t.string :recipe_ingredients
+      t.string :name
       t.string :meal_type
       t.string :recipe_instructions
 
