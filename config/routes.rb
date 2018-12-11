7d35d55e5cb1e180c72b3bef2870dc34e0359b23
nnannaegbujiobi <nnanna@newvybe.com>
2018-12-11 15:00:33 -0600

    routes workouts and meals

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+
+  namespace :api do
+    get "/workouts" => "workouts#index"
+
+    get "workouts/daily" => "workouts#show"
+
+    get "/user_workouts/:id" => "user_workouts#show"
+
+    post "/user_workouts" => "user_workouts#create"
+
+    #meals
+
+    get "/meals" => "meals#index"
+    get "/meals/new" => "meals#new"
+    get "/meals/:id" => "meals#show"
+    get "/meals/:id/edit" => "meals#edit"
+
+    
+end
 end
