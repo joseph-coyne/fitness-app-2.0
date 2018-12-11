40c0380aef8abef79212623ec4c24f9749e2b6fd
Joseph Coyne <39542045+josephpcoyne@users.noreply.github.com>
2018-12-11 16:37:49 -0500

    Update workout.rb

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,12 +1,17 @@
-class Workout < ApplicationRecord
-  def index
-    @workouts = Workout.all
-    render "index.json.jbuilder"
-  end
+class Api::WorkoutsController < ApplicationController
 
-  def show
-    workouts = Workout.where(level:current_user.level.downcase) 
-    @workout = workouts.sample
-    render "show.json.jbuilder"
+def index
+  @workouts = Workout.all
+  render "index.json.jbuilder"
+end
+
+def show
+  puts "======================#{current_user}"
+  workouts = Workout.where(level: current_user.level.downcase) 
+  @workout = workouts.sample
+  puts "======================#{@workout}"
+  # @workout = Workout.find_by(id:params[:id])
+  render "show.json.jbuilder"
+
   end
 end
