155da7722a35ee6d1b8ebccac17f373a798d4a7d
josephpcoyne <josephpcoyne@gmail.com>
2018-12-11 16:49:57 -0500

    adds tags controller and user_workouts

--- app/controllers/api/user_workouts_controller.rb
+++ app/controllers/api/user_workouts_controller.rb
@@ -1,2 +1,14 @@
-class Api::UserWorkoutsController < ApplicationController
+class Api::WorkoutsController < ApplicationController
+	def index
+	  @workouts = Workout.all
+	  render "index.json.jbuilder"
+	end
+
+	def show
+	  workouts = Workout.where(level: current_user.level.downcase) 
+	  @workout = workouts.sample
+	  # @workout = Workout.find_by(id:params[:id])
+	  render "show.json.jbuilder"
+
+	end
 end
