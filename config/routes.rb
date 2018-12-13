266e6be8d7e3fe5009a64bf1bfaee2351fa7a7ea
Joseph Coyne <39542045+josephpcoyne@users.noreply.github.com>
2018-12-13 16:52:01 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'git/add'
-  get 'git/.'
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
