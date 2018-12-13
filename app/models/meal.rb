24d8e93bf4c02c8300c30876b1aa6f3d5c3de573
MikeHenze <yashuachrist@gmail.com>
2018-12-13 16:27:09 -0500

    adding associations and model methods to meal model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,46 @@
 class Meal < ApplicationRecord
+
+  belongs_to :user
+  has_many :ingredient_meals, dependent: :destroy
+  has_many :ingredients, through: :ingredient_meals
+
+  validates :name, presence: true
+  validates :recipe_instructions, presence: true, length: {maximum: 200}
+
+  
+  def nutrients
+    total_protein = 0
+    total_carbohydrates = 0
+    total_fat = 0
+    total_magnesium = 0
+    total_zinc = 0
+    total_iron = 0
+    total_folate = 0
+    total_vitamin_d = 0
+    
+    ingredients.each do |ingredient|
+      # use the ingredient model method protein to get a sum
+      total_protein += ingredient.nutrients[:protein]
+      total_carbohydrates += ingredient.nutrients[:carbohydrates]
+      total_fat += ingredient.nutrients[:fat]
+      total_magnesium += ingredient.nutrients[:magnesium]
+      total_zinc += ingredient.nutrients[:zinc]
+      total_iron += ingredient.nutrients[:iron]
+      total_folate += ingredient.nutrients[:folate]
+      total_vitamin_d += ingredient.nutrients[:vitamin_d]
+    end
+    nutrients_hash = {
+      protein: total_protein,
+      carbohydrates: total_carbohydrates,
+      fat: total_fat,
+      magnesium: total_magnesium,
+      zinc: total_zinc,
+      iron: total_iron,
+      folate: total_folate,
+      vitamin_d: total_vitamin_d
+
+    }
+    return nutrients_hash
+  end
+  
 end
