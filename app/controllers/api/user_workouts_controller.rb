688f2bf2fc1eb072273140e713d183d4724804e9
nnannaegbujiobi <nnanna@newvybe.com>
2018-12-18 15:20:50 -0600

    user_workout controller

--- app/controllers/api/user_workouts_controller.rb
+++ app/controllers/api/user_workouts_controller.rb
@@ -1,14 +1,27 @@
-class Api::WorkoutsController < ApplicationController
-	def index
-	  @workouts = Workout.all
-	  render "index.json.jbuilder"
-	end
-
-	def show
-	  workouts = Workout.where(level: current_user.level.downcase) 
-	  @workout = workouts.sample
-	  # @workout = Workout.find_by(id:params[:id])
-	  render "show.json.jbuilder"
-
-	end
+class Api::UserWorkoutsController < ApplicationController
+  def show 
+    @user_workout = UserWorkout.find_by(id:params[:id])
+    render "show.json.jbuilder"
+  end
+  def create 
+    @user_workout = UserWorkout.find_by(user_id: current_user.id, workout_id: params[:workout_id])
+    # if user_workout exists..theyve completed the workout..increase times done of user_workout by 1
+    if @user_workout
+      @user_workout.times_done += 1
+      @user_workout.save
+      render "show.json.jbuilder"
+    else
+      @user_workout = UserWorkout.create(
+        user_id: current_user.id,
+        workout_id: params[:workout_id],
+        times_done: 1
+      )
+      if @user_workout.save
+        render "show.json.jbuilder"
+      else
+        render json: {errors: @user_workout.errors.full_messages}, status: :unprocessable_entity 
+      end
+    end
+    
+  end
 end
